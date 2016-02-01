811f1f605aee73af096fed6a28e8cb5f964c7032
İsmail Başaran <basaran.ismaill@gmail.com>
2016-02-01 17:02:11 +0200

    removed iplıgin interface

--- opt/ahenk/base/plugin/IPlugin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Author: İsmail BAŞARAN <ismail.basaran@tubitak.gov.tr> <basaran.ismaill@gmail.com>
-
- class IPlugin(object):
-     """docstring for IPlugin"""
-     def __init__(self, arg):
-         super(IPlugin, self).__init__()
-         self.arg = arg
