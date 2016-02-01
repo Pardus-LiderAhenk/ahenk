3ccb1d1d05faeae8788057f20ee6bec3ad4d96db
İsmail Başaran <basaran.ismaill@gmail.com>
2016-02-01 17:00:34 +0200

    ahenk service

---
+++ opt/ahenk/api/service/ps_util.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: İsmail BAŞARAN <ismail.basaran@tubitak.gov.tr> <basaran.ismaill@gmail.com>
+import psutil
+
+if __name__ == '__main__':
+    
---
+++ opt/ahenk/plugins/plugin1/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: İsmail BAŞARAN <ismail.basaran@tubitak.gov.tr> <basaran.ismaill@gmail.com>
+
+
+
+def run(val):
+    print "oo yeah plugin1 " + str(val)
