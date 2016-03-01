5e2a058412614ae3f578c06f89ee059dc5c1b31a
İsmail Başaran <basaran.ismaill@gmail.com>
2016-03-01 17:26:10 +0200

    Abstract command class

---
+++ opt/ahenk/base/plugin/AbstractCommand.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: İsmail BAŞARAN <ismail.basaran@tubitak.gov.tr> <basaran.ismaill@gmail.com>
+from base.Scope import Scope
+
+class AbstractCommand(object):
+    """docstring for AbstractCommand"""
+    def __init__(self):
+        super(AbstractCommand, self).__init__()
+        self.scope = Scope.getInstance()
+
+    def run():
+        # implement this metot from command
+        pass
+
+    def logger(self):
+        try:
+            return self.scope.getLogger()
+        except Exception as e:
+            print('Logger did not found')
+            return None
+
+    def addMessageResponseQueue(self):
+        # TODO implement response queue
+        pass
