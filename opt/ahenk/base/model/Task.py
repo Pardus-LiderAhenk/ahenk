6951f0b23880ad0da73df3435b79beaaa082c7bb
İsmail Başaran <basaran.ismaill@gmail.com>
2016-03-02 11:33:48 +0200

    model and database initial classes

---
+++ opt/ahenk/base/model/Task.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: İsmail BAŞARAN <ismail.basaran@tubitak.gov.tr> <basaran.ismaill@gmail.com>
+
+class Task(object):
+    """docstring for Task"""
+    def __init__(self,message):
+        super(Task, self).__init__()
+
+    def getPluginId(self):
+        # Not implemented yet
+        pass
+
+    def getcommandId(self):
+        # Not implemented yet
+        pass
