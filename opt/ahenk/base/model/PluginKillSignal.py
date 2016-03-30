fdb095cf7c8a7d90c04aabdfc518d66b47bff014
İsmail Başaran <basaran.ismaill@gmail.com>
2016-03-30 17:11:47 +0300

    Kill signal for reload plugins

---
+++ opt/ahenk/base/model/PluginKillSignal.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: İsmail BAŞARAN <ismail.basaran@tubitak.gov.tr> <basaran.ismaill@gmail.com>
+
+
+class PluginKillSignal(object):
+
+    @property
+    def obj_name(self):
+        return "KILL_SIGNAL"
