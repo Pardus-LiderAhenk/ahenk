c96fe5d2518e214497a53ad79bde7ec281e7ad4e
İsmail Başaran <basaran.ismaill@gmail.com>
2016-04-01 14:11:48 +0300

    initial scheduler impl

---
+++ opt/ahenk/base/scheduler/APSchedulerImpl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: İsmail BAŞARAN <ismail.basaran@tubitak.gov.tr> <basaran.ismaill@gmail.com>
+
+from base.scheduler.BaseScheduler import BaseScheduler
+
+class APSchedulerImpl(BaseScheduler):
+
+    def initialize(self):
+        # Not implemented yet
+        pass
+
+    def add_job(self):
+        # Not implemented yet
+        pass
+
+    def add_job_by_hour(self):
+        # Not implemented yet
+        pass
+
+    def add_job_by_mount(self):
+        # Not implemented yet
+        pass
+
+    def add_job_by_minute(self):
+        # Not implemented yet
+        pass
+
