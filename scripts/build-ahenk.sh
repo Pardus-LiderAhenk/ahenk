86d0e296a47fc30e20bc96eb6cd5921ac0f6f2f1
emre.akkaya <emre.akkaya@agem.com.tr>
2016-06-07 15:26:16 +0300

    add ahenk build script

---
+++ scripts/build-ahenk.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###
+# This script builds the project and generates Ahenk distribution (ahenk.deb)
+#
+# Generated file can be found under /tmp/ahenk
+###
+
+pushd $(dirname $0) > /dev/null
+PRJ_ROOT_PATH=$(dirname $(pwd -P))
+popd > /dev/null
+echo "Project path: $PRJ_ROOT_PATH"
+
+# Generate Ahenk packages
+echo "Generating Ahenk packages..."
+cd "$PRJ_ROOT_PATH"
+dpkg-buildpackage -b
+echo "Generated Ahenk packages"
+
+EXPORT_PATH=/tmp/ahenk
+echo "Export path: $EXPORT_PATH"
+
+# Copy resulting files
+echo "Copying generated Ahenk packages to $EXPORT_PATH..."
+mkdir -p "$EXPORT_PATH"
+cp -rf "$PRJ_ROOT_PATH"/../ahenk*.deb "$EXPORT_PATH"
+cp -rf "$PRJ_ROOT_PATH"/../ahenk*.changes "$EXPORT_PATH"
+echo "Copied generated Ahenk packages."
+
+echo "Built finished successfully!"
+echo "Files can be found under: $EXPORT_PATH"
