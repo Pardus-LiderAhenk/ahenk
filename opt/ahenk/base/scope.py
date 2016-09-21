4715574fe9d600072ff1c8b942f9e3bdeeac0900
Volkan Şahin <bm.volkansahin@gmail.com>
2016-09-21 11:47:47 +0300

    variables and functions name is changed to according to PEP8

--- opt/ahenk/base/scope.py
+++ opt/ahenk/base/scope.py
@@ -6,106 +6,106 @@
 class Scope(object):
     """docstring for Scope"""
 
-    scopeInstance = None
+    scope_instance = None
 
     def __init__(self):
         super(Scope, self).__init__()
-        self.customMap = dict()
-        self.configurationManager = None
-        self.messageManager = None
+        self.custom_map = dict()
+        self.configuration_manager = None
+        self.message_manager = None
         self.logger = None
-        self.pluginManager = None
-        self.taskManager = None
-        self.responseQueue = None
+        self.plugin_manager = None
+        self.task_manager = None
+        self.response_queue = None
         self.registration = None
-        self.eventManager = None
-        self.executionManager = None
-        self.dbService = None
+        self.event_manager = None
+        self.execution_manager = None
+        self.db_service = None
         self.messenger = None
         self.scheduler = None
 
     @staticmethod
-    def getInstance():
-        return scopeInstance
+    def get_instance():
+        return scope_instance
 
     @staticmethod
-    def setInstance(scopeObj):
-        global scopeInstance
-        scopeInstance = scopeObj
+    def set_instance(scope_obj):
+        global scope_instance
+        scope_instance = scope_obj
 
-    def getCustomMap(self):
-        return self.customMap
+    def get_custom_map(self):
+        return self.custom_map
 
-    def putCustomMap(self, name, value):
-        self.custom[str(name)] = value
+    def put_custom_map(self, name, value):
+        self.custom_map[str(name)] = value
 
-    def getCustomParam(self, name):
-        return self.customMap[str(name)]
+    def get_custom_param(self, name):
+        return self.custom_map[str(name)]
 
-    def getConfigurationManager(self):
-        return self.configurationManager
+    def get_configuration_manager(self):
+        return self.configuration_manager
 
-    def setConfigurationManager(self, configurationManager):
-        self.configurationManager = configurationManager
+    def set_configuration_manager(self, configuration_manager):
+        self.configuration_manager = configuration_manager
 
-    def getLogger(self):
+    def get_logger(self):
         return self.logger
 
-    def setLogger(self, logger):
+    def set_logger(self, logger):
         self.logger = logger
 
-    def getMessageManager(self):
-        return self.messageManager
+    def get_message_manager(self):
+        return self.message_manager
 
-    def setMessageManager(self, messageManager):
-        self.messageManager = messageManager
+    def set_message_manager(self, message_manager):
+        self.message_manager = message_manager
 
-    def getPluginManager(self):
-        return self.pluginManager
+    def get_plugin_manager(self):
+        return self.plugin_manager
 
-    def setPluginManager(self, pluginManager):
-        self.pluginManager = pluginManager
+    def set_plugin_manager(self, plugin_manager):
+        self.plugin_manager = plugin_manager
 
-    def getTaskManager(self):
-        return self.taskManager
+    def get_task_manager(self):
+        return self.task_manager
 
-    def setTaskManager(self, taskManager):
-        self.taskManager = taskManager
+    def set_task_manager(self, task_manager):
+        self.task_manager = task_manager
 
-    def getResponseQueue(self):
-        return self.responseQueue
+    def get_response_queue(self):
+        return self.response_queue
 
-    def setResponseQueue(self, responseQueue):
-        self.responseQueue = responseQueue
+    def set_response_queue(self, response_queue):
+        self.response_queue = response_queue
 
-    def getRegistration(self):
+    def get_registration(self):
         return self.registration
 
-    def setRegistration(self, registration):
+    def set_registration(self, registration):
         self.registration = registration
 
-    def getEventManager(self):
-        return self.eventManager
+    def get_event_manager(self):
+        return self.event_manager
 
-    def setEventManager(self, eventManager):
-        self.eventManager = eventManager
+    def set_event_manager(self, event_manager):
+        self.event_manager = event_manager
 
-    def getExecutionManager(self):
-        return self.executionManager
+    def get_execution_manager(self):
+        return self.execution_manager
 
-    def setExecutionManager(self, executionManager):
-        self.executionManager = executionManager
+    def set_execution_manager(self, execution_manager):
+        self.execution_manager = execution_manager
 
-    def getDbService(self):
-        return self.dbService
+    def get_db_service(self):
+        return self.db_service
 
-    def setDbService(self, dbService):
-        self.dbService = dbService
+    def set_sb_service(self, db_service):
+        self.db_service = db_service
 
-    def getMessenger(self):
+    def get_messenger(self):
         return self.messenger
 
-    def setMessenger(self, messenger):
+    def set_messenger(self, messenger):
         self.messenger = messenger
 
     def set_scheduler(self, scheduler):
