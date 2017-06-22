f1e72b43325971c8bf35bc4fd97f917a9ec8eabc
root <root@localhost.localdomain>
2017-06-22 21:53:07 +0300

    bugfix plugins

--- opt/ahenk/plugins/local-user/scripts/find_locked_users.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -n 's/^.*locked="\([A-Za-z0-9; ]*\)".*$/\1/p' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
--- opt/ahenk/plugins/usb/scripts/ENABLED_printer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-modprobe usblp
-service cups start
