4bec8505cd577102d2b2926e73e8f70cbc3c23fa
edip <edip@localhost.localdomain>
2018-11-14 11:35:33 +0300

    bugfix

--- src/ahenkd.py
+++ src/ahenkd.py
@@ -299,18 +299,20 @@ class AhenkDaemon(BaseDaemon):
 
         if conf_manager.has_section('MACHINE'):
             user_disabled = conf_manager.get("MACHINE", "user_disabled")
-            if user_disabled==0:
+            self.logger.info('User disabled value='+ str(user_disabled))
+            if user_disabled == '0':
                 self.logger.info('local user disabling')
                 global_scope.get_registration().disable_local_users()
 
-                conf_manager.set('MACHINE', 'user_disabled', 1)
+                conf_manager.set('MACHINE', 'user_disabled', '1')
 
                 with open('/etc/ahenk/ahenk.conf', 'w') as configfile:
                     self.conf_manager.write(configfile)
                 self.logger.info('local user disabled')
+                user_disabled = conf_manager.get("MACHINE", "user_disabled")
+                self.logger.info('User disabled value=' + str(user_disabled))
             else :
                 self.logger.info('users already disabled')
-
 
         #self.logger.info('Ahenk was registered')
 
